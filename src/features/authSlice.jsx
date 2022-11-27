9e14e328a38c6d1f4e98c237feb247ac30c8c0b2
duygukucukoglu35 <duygukucukoglu35@gmail.com>
2022-11-27 16:12:00 +0300

    add authslice

---
+++ src/features/authSlice.jsx
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+
+const initialState={
+    user: null
+}
+//slice=> redux gibi ayrı ayrı klasör kurma
+//action,type,reducer bu slice(dilim) içinde tanımla
+//slice içerisine 3 method alır
+//name:type | reducers:reducer | initialState:yukarıdaki tanımlanan stateden.
+//action={type,payload} : type=name olduğundan type yazmaya gerek yok.
+//return olmadığı için state.user=null yazarsın
+const authSlice=createSlice({
+    name: "auth",
+    initialState,
+    reducers:{
+        setUser:(state, {payload})=>{
+            state.user=payload;
+        },
+        clearUser:(state)=>{
+            state.user=null;
+        }
+
+    }
+})
+//Redux Toolkit action otomatik veriyor, .actions
+//hangi fn için istersen desc ederek export et=> .actions
+export const{setUser,clearUser}=authSlice.actions
+
+//Reducers için export etme=> .reducer
+export default authSlice.reducer
